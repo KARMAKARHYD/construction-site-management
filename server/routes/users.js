5ba848a35b5cb33e8fba7dfa325d5b1348285cf4
karmakar.193@gmail.com <karmakar.193@gmail.com>
2025-07-08 20:16:53 +0530

    Initial setup

---
+++ server/routes/users.js
@@ -0,0 +1,87 @@
+const router = require('express').Router();
+let User = require('../models/user.model');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+router.route('/register').post((req, res) => {
+  const { username, password, role } = req.body;
+
+  if (!username || !password || !role) {
+    return res.status(400).json({ msg: 'Please enter all fields' });
+  }
+
+  User.findOne({ username })
+    .then(user => {
+      if (user) return res.status(400).json({ msg: 'User already exists' });
+
+      const newUser = new User({
+        username,
+        password,
+        role
+      });
+
+      bcrypt.genSalt(10, (err, salt) => {
+        bcrypt.hash(newUser.password, salt, (err, hash) => {
+          if (err) throw err;
+          newUser.password = hash;
+          newUser.save()
+            .then(user => {
+              jwt.sign(
+                { id: user.id },
+                process.env.JWT_SECRET,
+                { expiresIn: 3600 },
+                (err, token) => {
+                  if (err) throw err;
+                  res.json({
+                    token,
+                    user: {
+                      id: user.id,
+                      username: user.username,
+                      role: user.role
+                    }
+                  });
+                }
+              )
+            })
+            .catch(err => res.status(400).json('Error: ' + err));
+        });
+      });
+    });
+});
+
+router.route('/login').post((req, res) => {
+  const { username, password } = req.body;
+
+  if (!username || !password) {
+    return res.status(400).json({ msg: 'Please enter all fields' });
+  }
+
+  User.findOne({ username })
+    .then(user => {
+      if (!user) return res.status(400).json({ msg: 'User does not exist' });
+
+      bcrypt.compare(password, user.password)
+        .then(isMatch => {
+          if (!isMatch) return res.status(400).json({ msg: 'Invalid credentials' });
+
+          jwt.sign(
+            { id: user.id },
+            process.env.JWT_SECRET,
+            { expiresIn: 3600 },
+            (err, token) => {
+              if (err) throw err;
+              res.json({
+                token,
+                user: {
+                  id: user.id,
+                  username: user.username,
+                  role: user.role
+                }
+              });
+            }
+          )
+        });
+    });
+});
+
+module.exports = router;
